f4075c9fdf8b2b8acc20d86a05df6a10d18c31f1
ba72 <weihongcheng.work@foxmail.com>
2023-12-21 17:10:59 +0800

    test

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -25,7 +25,6 @@ import static java.awt.AWTEventMulticaster.add;
  */
 public class GameController implements GameListener, Serializable {
 
-
     private int step=10;
 
     private Chessboard model;
